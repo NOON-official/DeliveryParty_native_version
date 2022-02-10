5a5f0c85a766eda404b026abf758f899b4e1fdfb
YeonSeong-Lee <dltjddus3@gmail.com>
2022-02-11 01:07:30 +0900

    [CHORE] 코딩 컨벤션 맞추기

--- components/MainPage.js
+++ components/MainPage.js
@@ -18,58 +18,55 @@ export default function MainPage() {
   
 const [modalVisible, setModalVisible] = useState(false);
 
-const [Datas,setDatas]=useState([]);
- useEffect(()=>{
-   UploadCollection.onSnapshot(snapshot=>{
-setDatas(snapshot.docs.map(doc=>({id:(doc.id),Data:(doc.data())})))
-   })
+const [Datas,setDatas] = useState([]);
+
+useEffect(() =>
+{
+  UploadCollection.onSnapshot( snapshot => 
+    {
+      setDatas(snapshot.docs.map(doc=>({id:(doc.id),Data:(doc.data())})))
+    })
  },[])
-
-
-    return (
-  
-        <View style={styles.container}>
-              <View style={styles.mainTop}>
-                <Text style={styles.mainTopText}>오늘의 파티</Text>
-
-              </View>
-
-            <SafeAreaView style={styles.mainMiddle}>
-            <ScrollView style={{flex: 1 }} class="container" >
-         {Datas.map((item)=>{
-           return( 
-           <TouchableOpacity onPress={()=>setModalVisible(true)} style={styles.list} >
-                    <Modal
-                   animationType="slide"
-                    transparent={true}
-                    visible={modalVisible}
-                     onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
-          setModalVisible(!modalVisible);
-        }}
-      ></Modal>
-            <View style={styles.mainMiddleView1}>
-              <View style={styles.mainMiddleIcon}><Text>icon</Text></View>
-            <Text style={styles.mainMiddleText1}> 5/5</Text>
-              </View>  
-
-              <View style={styles.mainMiddleView2}>
-              <Text style={styles.mainMiddleTitle}>{item.Data.Title}</Text>
-           
-                   <View style={styles.mainMiddleView3}>
-                        <Text style={styles.mainMiddleName}>name</Text>
-                    <Text style={styles.mainMiddleTime}>{item.Data.Created}</Text>
-              </View>
-              </View>
-            </TouchableOpacity>)})}
-       
- 
-            </ScrollView>
-            </SafeAreaView>
-           
-            <StatusBar style="auto"/>
-
-        </View>
+return
+(  
+<View style={styles.container}>
+      <View style={styles.mainTop}>
+         <Text style={styles.mainTopText}>오늘의 파티</Text>
+      </View>
+      <SafeAreaView style={styles.mainMiddle}>
+      <ScrollView style={{flex: 1 }} class="container" >
+    {Datas.map((item) => {
+      return( 
+      <TouchableOpacity onPress={()=>setModalVisible(true)} style={styles.list} >
       
+      <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => 
+      {
+      Alert.alert("Modal has been closed.");
+      setModalVisible(!modalVisible);
+      }}> 
+      </Modal>
+      <View style={styles.mainMiddleView1}>
+            <View style={styles.mainMiddleIcon}>
+              <Text>icon</Text>
+            </View>
+              <Text style={styles.mainMiddleText1}> 5/5</Text>
+            </View>  
+            <View style={styles.mainMiddleView2}>
+              <Text style={styles.mainMiddleTitle}>{item.Data.Title}</Text>
+              <View style={styles.mainMiddleView3}>
+                <Text style={styles.mainMiddleName}>name</Text>
+                <Text style={styles.mainMiddleTime}>{item.Data.Created}</Text>
+              </View>
+            </View>
+    </TouchableOpacity>)})}
+    </ScrollView>
+    </SafeAreaView>
+    
+    <StatusBar style="auto"/>
+  </View>
     );
 };
--- styles/UploadPageStyle.js
+++ styles/UploadPageStyle.js
@@ -1,43 +1,38 @@
-import { StyleSheet, Dimensions } from "react-native";
-const {width: SCREEN_WIDTH } = Dimensions.get("window");
-const {height: SCREEN_HEIGHT } = Dimensions.get("window");
+import {StyleSheet, Dimensions} from 'react-native';
+const {width: SCREEN_WIDTH} = Dimensions.get('window');
+const {height: SCREEN_HEIGHT} = Dimensions.get('window');
+
 export const styles = StyleSheet.create({
-    container:{
-    flex:1
-    },
-    header:{
-        flex: 0.6,
-        borderBottomColor: 'black',
-        borderBottomWidth: 1,
-        flexDirection:"row",
-        marginTop:20    
-    },
-    headerTitle:{
-     
-      fontSize:22,
-      fontWeight:"bold",
-      color:'black',
-      textAlign:"center",
-     
-    
-    },
-    headerButton:{
-    
-      borderRadius: 80,
-      color:"#FF480E",
-    },
-    description:{
-    flex:6,
-  
-    },
-  image:{
+  container: {
+    flex: 1,
+  },
+  header: {
+    flex: 0.6,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  headerTitle: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+  },
+  headerButton: {
+    borderRadius: 80,
+    color: '#FF480E',
+  },
+  description: {
+    flex: 6,
+  },
+  image: {
     width: SCREEN_WIDTH * 0.9,
-      flex:0.5,
-      borderTopColor: '#777777',
-      borderTopWidth: 1,
-      marginLeft:15,
-      justifyContent:"center"
-      
+    flex: 0.5,
+    borderTopColor: '#777777',
+    borderTopWidth: 1,
+    marginLeft: 15,
+    justifyContent: 'center',
   },
   textIput: {
     width: SCREEN_WIDTH * 0.9,
@@ -47,14 +42,13 @@ export const styles = StyleSheet.create({
     color: '#777777',
     borderBottomColor: '#777777',
     borderBottomWidth: 1,
-    padding: 10
-},textIputDescription: {
-  width: SCREEN_WIDTH * 0.9,
-  margin: 10,
-  fontSize: 20,
-  fontWeight: 'bold',
-  color: '#777777',
-  
-},
-
-})
+    padding: 10,
+  },
+  textIputDescription: {
+    width: SCREEN_WIDTH * 0.9,
+    margin: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#777777',
+  },
+});
